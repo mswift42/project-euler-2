7ef156131a94554019c0628af27fbd115507d099
mswift42 <martin.haesler@gmail.com>
2013-05-03 18:01:54 +0100

    Changed euler92 to lookup square sums in a hash-table.

--- euler92.lisp
+++ euler92.lisp
@@ -4,14 +4,24 @@
 (defun square-sum (n)
   (reduce #'+ (mapcar #'(lambda (x) (* x x)) (number-to-list n))))
 
+(defparameter *ends-in-hash* (make-hash-table))
+
+(defun init-hash ()
+  (loop
+       for i from 1 to 600
+     do ( setf ( gethash i *ends-in-hash*) (ends-in i))))
+
 (defun ends-in (n)
   (loop
-       for x1 = (square-sum n) then (square-sum x1)
+       for x1 = (or (gethash n *ends-in-hash*) (square-sum n))
+                then (or (gethash x1 *ends-in-hash*) (square-sum x1))
        when (or (= x1 89) (= x1 1))
        do (return (values x1))))
 
-(defun result ()
+(defun euler-92 ()
+  (init-hash)
   (loop
        for i from 2 to 10000000
        when (= (ends-in i) 89)
        counting i))
+
